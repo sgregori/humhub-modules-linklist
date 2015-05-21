ca2862cd4733181ddb2e0e1b2d61ee09ddeca840
SalvaG <sgregori@users.noreply.github.com>
2015-05-21 16:54:10 +0200

    Fixed some translation bugs

--- views/linklist/editCategory.php
+++ views/linklist/editCategory.php
@@ -12,11 +12,11 @@
 
 
 <div class="panel panel-default">
-    <div class="panel-heading"><strong>Create</strong> new Category</div>
+    <div class="panel-heading"><b><?php echo Yii::t('LinklistModule.base', 'Create new Category'); ?></b></div>
     <div class="panel-body">
     	<?php if($isCreated) { ?>
     	<?php } else { ?>
-    	<p>Edit Category</p>	
+    	<p><b><?php echo Yii::t('LinklistModule.base', 'Edit Category'); ?></b></p>	
 		<?php }
         $form = $this->beginWidget('CActiveForm', array(
             'id' => 'category-edit-form',
@@ -25,24 +25,27 @@
 		//echo $form->errorSummary($category); ?>
 
 	    <div class="form-group">
-	        <?php echo $form->labelEx($category, 'title'); ?>
-	        <?php echo $form->textField($category, 'title', array('class' => 'form-control')); ?>
+	        <?php //echo $form->labelEx($category, 'title'); ?>
+	        <?php echo $form->textField($category, 'title', array('class' => 'form-control'),'placeholder' => Yii::t('LinklistModule.base', 'Title') )     ); ?>
+
 	        <?php echo $form->error($category, 'title'); ?>
 	    </div>
 	    
 	    <div class="form-group">
-	        <?php echo $form->labelEx($category, 'description'); ?>
-	        <?php echo $form->textArea($category, 'description', array('class' => 'form-control', 'rows' => 3)); ?>
+	        <?php //echo $form->labelEx($category, 'description'); ?>
+	        <?php echo $form->textArea($category, 'description', array('class' => 'form-control', 'rows' => 3, 'placeholder' => Yii::t('LinklistModule.base', 'Description') )     ); ?>
+
 	        <?php echo $form->error($category, 'description'); ?>
 	    </div>
 	    
 		<div class="form-group">
-	        <?php echo $form->labelEx($category, 'sort_order'); ?>
-	        <?php echo $form->numberField($category, 'sort_order', array('class' => 'form-control')); ?>
+	        <?php //echo $form->labelEx($category, 'sort_order'); ?>
+	        <?php echo $form->numberField($category, 'sort_order', array('class' => 'form-control', 'placeholder' => Yii::t('LinklistModule.base', 'Sort Order') )     ); ?>
+
 	        <?php echo $form->error($category, 'sort_order'); ?>
 	    </div>
 	    
-        <?php echo CHtml::submitButton('Save', array('class' => 'btn btn-primary')); ?>
+        <?php echo CHtml::submitButton( Yii::t('LinklistModule.base', 'Save') , array('class' => 'btn btn-primary')); ?>
         
         <?php $this->endWidget(); ?>
     </div>
