97f24d277f0c9941bcb07e92f58c0f82f7ceee79
SalvaG <sgregori@users.noreply.github.com>
2015-05-21 16:50:21 +0200

    Fixed some translation bugs

--- views/linklist/editLink.php
+++ views/linklist/editLink.php
@@ -14,9 +14,9 @@
 <div class="panel panel-default">
     <div class="panel-body">
     	<?php if($isCreated) { ?>
-    	<p><?php echo Yii::t('LinklistModule.base', 'Create new Link'); ?></p>
+    	<p><b><?php echo Yii::t('LinklistModule.base', 'Create new Link'); ?></b></p>
     	<?php } else { ?>
-    	<p><?php echo Yii::t('LinklistModule.base', 'Edit Link'); ?></p>	
+    	<p><b><?php echo Yii::t('LinklistModule.base', 'Edit Link'); ?></b></p>	
 		<?php }
         $form = $this->beginWidget('CActiveForm', array(
             'id' => 'link-edit-form',
@@ -25,26 +25,26 @@
 		//echo $form->errorSummary($link); ?>
 
 	    <div class="form-group">
-	        <?php echo $form->labelEx($link, 'title'); ?>
-	        <?php echo $form->textField($link, 'title', array('class' => 'form-control')); ?>
+	        <?php //echo $form->labelEx($link, 'title'); ?>
+	        <?php echo $form->textField($link, 'title' , array('class' => 'form-control','placeholder' => Yii::t('LinklistModule.base', 'Title') )     ); ?>
 	        <?php echo $form->error($link, 'title'); ?>
 	    </div>
 	    
 	    <div class="form-group">
-	        <?php echo $form->labelEx($link, 'description'); ?>
-	        <?php echo $form->textArea($link, 'description', array('class' => 'form-control', 'rows' => '2')); ?>
+	        <?php //echo $form->labelEx($link, 'description'); ?>
+	        <?php echo $form->textArea($link, 'description', array('class' => 'form-control', 'rows' => '2' ,'placeholder' => Yii::t('LinklistModule.base', 'Description') )     ); ?>
 	        <?php echo $form->error($link, 'description'); ?>
 	    </div>
 	    
     	<div class="form-group">
-	        <?php echo $form->labelEx($link, 'href'); ?>
-	        <?php echo $form->textField($link, 'href', array('class' => 'form-control')); ?>
+	        <?php //echo $form->labelEx($link, 'href'); ?>
+	        <?php echo $form->textField($link, 'href', array('class' => 'form-control', 'placeholder' => Yii::t('LinklistModule.base', 'Link') )     ); ?>
 	        <?php echo $form->error($link, 'href'); ?>
 	    </div>
 	    
 		<div class="form-group">
-	        <?php echo $form->labelEx($link, 'sort_order'); ?>
-	        <?php echo $form->numberField($link, 'sort_order', array('class' => 'form-control')); ?>
+	        <?php //echo $form->labelEx($link, 'sort_order'); ?>
+	        <?php echo $form->numberField($link, 'sort_order', array('class' => 'form-control' ,'placeholder' => Yii::t('LinklistModule.base', 'Sort Order') )     ); ?>
 	        <?php echo $form->error($link, 'sort_order'); ?>
 	    </div>
 	    
